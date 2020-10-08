d047f5f42d18f42ed1b030317e5477d2c970f0e4
Ruslan Boom <rusik.lim@yandex.ru>
2020-10-09 00:27:47 +0300

    Добавьте файлы проекта.

---
+++ biba5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Средний
+            try
+            {
+                Console.Write("Введите N:");
+                int n = int.Parse(Console.ReadLine());
+                if (n % 2 == 0 && n % 7 == 0 && n % 13 != 0 && n % 11 != 0) Console.WriteLine("Истина");
+                else Console.WriteLine("Ложь");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
